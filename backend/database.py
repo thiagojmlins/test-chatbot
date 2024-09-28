1c9c82aabd2e19ecfab29ce49ff49ee446023764
Thiago Lins <thiagojmlins@gmail.com>
2024-09-28 17:09:25 -0300

    dockerize

--- backend/database.py
+++ backend/database.py
@@ -1,9 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./chatbot.db"
+SQLALCHEMY_DATABASE_URL = "postgresql://postgres:postgres@db/chatbot"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
